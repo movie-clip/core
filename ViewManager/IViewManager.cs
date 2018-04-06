6145dc834c0dcb737069b42222f340224358d133
AntonAmetov <anton@digitgaming.com>
2018-04-06 13:47:47 +0300

    IViewManager interface

--- ViewManager/IViewManager.cs
+++ ViewManager/IViewManager.cs
@@ -3,5 +3,11 @@ namespace Core.ViewManager
 {
     public interface IViewManager
     {
+        void Init();
+        
+        void RegisterView(string viewId, string layerId);
+
+        BaseView SetView(string viewId, object options = null);
+        
     }
 }
